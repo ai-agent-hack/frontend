cef0f4aef8ce65f856bcf7b7a2650a7f8c71b226
ryota <kurokawa.ryota.1124@gmail.com>
2025-06-23 08:35:22 +0900

    Input (#6)
    * tailwindないんかぁい
    
    * add Mastra & chat (#1)
    
    * init mastra
    
    * add chat
    
    * fix handle mastra.db
    
    * fix chat use mastra
    
    * add debug mastra
    
    * use vertex
    
    * add env
    
    * Update README.md
    
    * Fix GOOGLE_APPLICATION in README
    
    * Add ChakraProvider and index page
    
    * Rename workflow directory name
    
    * Update workflow yml files
    
    * Fix installing bun in CI
    
    * Setup formatting commands
    
    * ChatベースのAIロジックを書くためのインターフェースとその通信、状態管理を実装 (#2)
    
    * add first workflow
    
    * fix route
    
    * use useObject
    
    * チャットでmessageとおすすめスポットの型をやりとりできるようにインターフェースを実装
    
    * 誰のメッセージか表示
    
    * fix ui
    
    * Refactor codes
    
    * Fix importing error and formatting
    
    * Migrate biome to 2.0.0
    
    * Add key.json in building workflow
    
    * Remove building workflow
    
    ---------
    
    Co-authored-by: Yu Takahashi <65745715+taka-2120@users.noreply.github.com>
    Co-authored-by: taka-2120 <yu_tkhs@icloud.com>
    
    * chakra replace
    
    * git が駄々こねてた
    
    * feat: ui
    
    * claudeの方が賢いんかな〜
    
    * agentに感動した。
    
    * いい感じにできた
    
    * いい感じにできた
    
    * レビュー対応
    
    * Remove unused file
    
    * Add bun.lockb to gitignore
    
    * Apply format
    
    ---------
    
    Co-authored-by: huku <108620445+hukurou0@users.noreply.github.com>
    Co-authored-by: taka-2120 <yu_tkhs@icloud.com>
    Co-authored-by: Yu Takahashi <65745715+taka-2120@users.noreply.github.com>

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,14 @@ const nextConfig: NextConfig = {
 		optimizePackageImports: ["@chakra-ui/react"],
 	},
 	serverExternalPackages: ["@mastra/*"],
+	async rewrites() {
+		return [
+			{
+				source: "/api/v1/:path*",
+				destination: "http://localhost:8000/api/v1/:path*",
+			},
+		];
+	},
 };
 
 export default nextConfig;
